7d6e2e22b4ff1bd87dbecae9543361c936911623
VinlaRose <109898811+VinlaRose@users.noreply.github.com>
2022-07-30 18:24:49 +0530

    updated

--- app.js
+++ app.js
@@ -3,13 +3,42 @@
 // alert("how are you?" + username) //give output
 
 var btnTranslate = document.querySelector("#btn-translate");
-var txtInput = document.querySelector("#text-input")
-console.log(txtInput)
+var txtInput = document.querySelector("#text-input");
+var outputDiv = document.querySelector("#output");
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
+// outputDiv.innerText = "vinla";
 
-function clickHandler(){
-    console.log("clicked");
-    console.log("input", txtInput.value);
+console.log(outputDiv);
+
+// console.log(txtInput)
+function getTranslationURL(text){
+    return serverURL + "?" + "text=" + text
 }
+
+
+function errorHandler(error){
+    console.log("error occurred", error);
+    alert("something wrong with server: try again after some time")
+
+}
+function clickHandler(){
+    var inputText = txtInput.value; 
+
+    //calling server for processing
+    fetch(getTranslationURL(inputText))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText;
+
+        } 
+        )
+        .catch(errorHandler)
+}; 
+
+
+
+
 
 btnTranslate.addEventListener("click", clickHandler)
 
